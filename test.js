2d273f6f0dd09e05352fc6d9b99fa6473de75692
Pimlada Chow <pimlada.ct@gmail.com>
2020-10-23 21:38:32 -0700

    Node Callback Hell and Promises

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-const MongoClient = require('mongodb').MongoClient; 
-const dboper = require('./operations');
-
-const url = 'mongodb://localhost:27017/'; 
-const dbname = 'nucampsite'; 
-                                                      
 MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
 
     console.log('Connected correctly to server');
@@ -11,11 +5,11 @@ MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
     const db = client.db(dbname);
 
     db.dropCollection('campsites')
-    .then(result => {   
-        console.log('Dropped Collection:', result);    
+    .then(result => {
+        console.log('Dropped Collection:', result);
     })
     .catch(err => console.log('No collection to drop.'));
-        
+
     dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites')
     .then(result => {
         console.log('Insert Document:', result.ops);
@@ -28,7 +22,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
         return dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },
             { description: "Updated Test Description" }, 'campsites');
     })
-    .then(result => {         
+    .then(result => {
         console.log('Updated Document Count:', result.result.nModified);
 
         return dboper.findDocuments(db, 'campsites');
@@ -41,7 +35,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }).then(client => {
     })
     .then(result => {
         console.log('Deleted Document Count:', result.deletedCount);
-        
+
         return client.close();
     })
     .catch(err => {
